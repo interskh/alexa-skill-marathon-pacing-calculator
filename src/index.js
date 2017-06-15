906233fa18f78f151d3aa5e47ecbf64ae68f5637
Kyle Sun <kyle@bigkyle.com>
2017-06-15 10:14:42 -0700

    Fix some edge cases like 0 day and multi day pace calculation

--- src/index.js
+++ src/index.js
@@ -26,20 +26,23 @@ exports.handler = function(event, context, callback) {
     alexa.execute();
 };
 
-var iso8601DurationRegex = /(-)?P(?:([\.,\d]+)Y)?(?:([\.,\d]+)M)?(?:([\.,\d]+)W)?(?:([\.,\d]+)D)?T(?:([\.,\d]+)H)?(?:([\.,\d]+)M)?(?:([\.,\d]+)S)?/;
+var iso8601DurationRegex = /(-)?P(?:([\.,\d]+)Y)?(?:([\.,\d]+)M)?(?:([\.,\d]+)W)?(?:([\.,\d]+)D)?T?(?:([\.,\d]+)H)?(?:([\.,\d]+)M)?(?:([\.,\d]+)S)?/;
 
 var parseISO8601Duration = function (iso8601Duration) {
-    var matches = iso8601Duration.match(iso8601DurationRegex);
+  var matches = iso8601Duration.match(iso8601DurationRegex);
+  if (matches == null) {
+    return 0;
+  }
 
-    //sign: matches[1] === undefined ? '+' : '-',
-    //years: matches[2] === undefined ? 0 : matches[2],
-    //months: matches[3] === undefined ? 0 : matches[3],
-    //weeks: matches[4] === undefined ? 0 : matches[4],
-    var days = matches[5] === undefined ? 0 : matches[5];
-    var hours = matches[6] === undefined ? 0 : matches[6];
-    var minutes = matches[7] === undefined ? 0 : matches[7];
-    var seconds = matches[8] === undefined ? 0 : matches[8];
-    return days*24*60*60*60 + hours*60*60 + minutes*60 + seconds;
+  //sign: matches[1] === undefined ? '+' : '-',
+  //years: matches[2] === undefined ? 0 : matches[2],
+  //months: matches[3] === undefined ? 0 : matches[3],
+  //weeks: matches[4] === undefined ? 0 : matches[4],
+  var days = matches[5] === undefined ? 0 : matches[5];
+  var hours = matches[6] === undefined ? 0 : matches[6];
+  var minutes = matches[7] === undefined ? 0 : matches[7];
+  var seconds = matches[8] === undefined ? 0 : matches[8];
+  return days*24*60*60*60 + hours*60*60 + minutes*60 + seconds;
 };
 
 var milePace = function(dur) {
@@ -73,7 +76,12 @@ var handlers = {
       var durSlot = this.event.request.intent.slots["dur"];
       if (durSlot && durSlot.value) {
         var dur = parseISO8601Duration(durSlot.value);
-        this.emit(':tell', RESULT_MESSAGE + paceToStr(milePace(dur)) + " per mile or " + paceToStr(kmPace(dur)) + " per km");
+        console.log("dur: " + dur);
+        if (dur > 0) {
+          this.emit(':tell', RESULT_MESSAGE + paceToStr(milePace(dur)) + " per mile or " + paceToStr(kmPace(dur)) + " per km");
+        } else {
+          this.emit(':tell', SORRY_MESSAGE);
+        }
       } else {
         this.emit(':tell', SORRY_MESSAGE);
       }
